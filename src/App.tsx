29dd324b170dc3b3309814d2d657920771bfcc81
danzycool <bizwithdan@gmail.com>
2023-04-03 00:02:07 +0100

    started implementing alert function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  return <div><ListGroup /></div>
+  return (
+    <div>
+      <Alert text="Hello World" />
+    </div>
+  );
 }
 
 export default App;
